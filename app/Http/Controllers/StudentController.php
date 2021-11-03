a3d324a614c16aafb71fa04db246fb5a5d1a406f
Jihanra <jihanrahadatulasy@gmail.com>
2021-11-03 14:07:11 +0700

    Membuat relasi mant to many

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -138,5 +138,10 @@ class StudentController extends Controller
         $student = student::where('name', 'like', "%" . $keyword . "%")->paginate(5);
         return view('students.index', compact('student'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
+    public function detail($id)
+    {
+        $student = Student::find($id);
+        return view('students.detail', ['student'=>$student]);
+    }
 
 }
